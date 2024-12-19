2a3572fe481a4b93c34212b13518daf151be6401
Asala El Ghandour <asala.elghandour@gmail.com>
2024-12-19 23:52:06 +0200

    adding Navbar styling, and home page text

--- src/Pages/Home/HomePage.tsx
+++ src/Pages/Home/HomePage.tsx
@@ -1,3 +1,17 @@
+import { Box, Typography } from "@mui/material";
+
 export function HomePage() {
-  return <div>home-page</div>;
+  return (
+    <Box
+      alignItems="center"
+      justifyContent="center"
+      display="flex"
+      height="100vh"
+      alignContent="center"
+    >
+      <Typography variant="h1" component="h2" textAlign="center">
+        Welcome
+      </Typography>
+    </Box>
+  );
 }
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,15 +1,55 @@
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import Link from "@mui/material/Link";
+import Logo from "../../assets/logo-white.png";
+import { Box } from "@mui/material";
 
 export function NavBar() {
   return (
-    <Breadcrumbs aria-label="breadcrumb">
-      <Link underline="hover" color="inherit" href="/">
-        Home
-      </Link>
-      <Link underline="hover" color="inherit" href="/suppliers">
-        Suppliers
-      </Link>
-    </Breadcrumbs>
+    <Box
+      m={0}
+      p={2}
+      sx={{
+        flexDirection: "row",
+        display: "flex",
+        position: "absolute",
+        top: 0,
+        justifyContent: "space-between",
+      }}
+      alignItems="center"
+      justifyContent="space-between"
+      height="15vh"
+      width="100vw"
+      bgcolor="rgb(27, 117, 183)"
+    >
+      <img src={Logo} height="80vh" alt="logo" />
+      <Box mr={5} alignItems="center">
+        <Breadcrumbs aria-label="breadcrumb">
+          <Link
+            sx={{
+              textDecoration: "none",
+              "&:hover": {
+                color: "#DCDCDE",
+              },
+            }}
+            color="#fff"
+            href="/"
+          >
+            Home
+          </Link>
+          <Link
+            sx={{
+              textDecoration: "none",
+              "&:hover": {
+                color: "#DCDCDE",
+              },
+            }}
+            color="#fff"
+            href="/suppliers"
+          >
+            Suppliers
+          </Link>
+        </Breadcrumbs>
+      </Box>
+    </Box>
   );
 }
