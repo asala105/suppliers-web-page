be63d7bc7d6e466bb1a1754f17ad34c2b81770eb
Asala El Ghandour <asala.elghandour@gmail.com>
2024-12-24 12:43:32 +0200

    adding update and delete api requests

---
+++ src/API/deleteSuplier.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { Config } from "../enviroment.config";
+
+const backend_url = Config.BACKEND_URL;
+export async function addSupplier(id: string) {
+  try {
+    const response = await axios.delete(`${backend_url}/supplier/${id}`);
+    return response.status === 200;
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+}
---
+++ src/API/updateSupplier.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { Config } from "../enviroment.config";
+import { EditSupplierInput, Supplier } from "../common/type";
+
+const backend_url = Config.BACKEND_URL;
+export async function addSupplier(data: EditSupplierInput) {
+  try {
+    const response = await axios.post(`${backend_url}/update-supplier`, {
+      id: data.id,
+      data: {
+        name: data.name,
+        vatNumber: data.vatNumber,
+      },
+    });
+    return response.data as Supplier[];
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
+}
